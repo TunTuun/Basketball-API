993dc815e9c3b8b1bbaf71a6a2bae16f8cdfbb7b
TunTuun <timur.beast@gmail.com>
2021-01-15 11:47:55 +0300

    guard fix

--- src/app/guards/team.guard.ts
+++ src/app/guards/team.guard.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
+import { CacheService } from '../services/cache.service';
 
 import { TeamsService } from '../services/teams.service';
 
 @Injectable()
 export class TeamGuard implements CanActivate {
-  constructor(private teamsService: TeamsService, private router: Router) { }
+
+  constructor(
+    private teamsService: TeamsService,
+    private router: Router,
+    private cacheService: CacheService) { }
 
   canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
-    if (this.teamsService.teamExists(route.params.team)) {
+    if (this.cacheService.getCacheData('teams') && this.teamsService.teamExists(route.params.team)) {
       return true;
     } else {
       return this.router.parseUrl('/teams');
